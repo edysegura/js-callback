06d46072dab3f37eeed9f61af1830fb4992ddef7
Edy Segura <ednardo@inatel.br>
2018-04-16 23:55:19 -0300

    Implemented a progress bar This closes #1

--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
 'use strict'
 
-const pokemonsNumber = Array.from(Array(251))
-  .map((value, index) => String(index + 1).padStart(3, '0'))
+const pokemonsNumber = Array.from(Array(251).keys())
+  .map(number => String(number + 1).padStart(3, '0'))
+
+let loaded = 0
+const total = pokemonsNumber.length
+const progressBar = document.querySelector('progress')
+const progressPercentage = document.querySelector('span')
+
+function updateProgressbar() {
+  let percentage = 0
+  ++loaded
+  percentage = Math.floor((loaded * 100) / total)
+  progressBar.value = percentage
+  progressPercentage.textContent = percentage + '%'
+}
 
 function createImageElement(pokemonNumber) {
   const img = new Image()
   img.src = `images/pokemons/${pokemonNumber}.png`
-  img.onload = () => console.log('Pokemon Loaded ' + pokemonNumber)
-  img.onerror = () => console.log('Pokemon failed ' + pokemonNumber)
+  img.addEventListener('load', updateProgressbar)
+  img.addEventListener('load', () => console.log('Pokemon Loaded ' + pokemonNumber))
+  img.addEventListener('error', () => console.log('Pokemon failed ' + pokemonNumber))
   document.body.appendChild(img)
 }
 
