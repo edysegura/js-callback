0d46dc65d89da06621e2071dc785be685f3e7956
Edy Segura <edysegura@gmail.com>
2018-11-20 23:05:33 -0200

    Refactoring load-in-order.js

--- js/load-in-order.js
+++ js/load-in-order.js
@@ -1,30 +1,34 @@
 'use strict'
 
-const pokemonsNumber = Array.from(Array(251).keys())
+const pokemonsNumber = Array
+  .from(Array(251).keys())
   .map(number => String(number + 1).padStart(3, '0'))
 
-let loaded = 0
-const total = pokemonsNumber.length
-const progressBar = document.querySelector('progress')
-const progressPercentage = document.querySelector('span')
-
 function updateProgressbar() {
-  let percentage = 0
-  ++loaded
-  percentage = Math.floor((loaded * 100) / total)
-  progressBar.value = percentage
-  progressPercentage.textContent = percentage + '%'
+  const progressBar = document.querySelector('progress')
+  const progressPercentage = document.querySelector('span')
+  const total = pokemonsNumber.length
+  let loaded = 0
+
+  return () => {
+    ++loaded
+    let percentage = Math.floor((loaded * 100) / total)
+    progressBar.value = percentage
+    progressPercentage.textContent = percentage + '%'
+  }
 }
 
 function loadInOrder() {
   const pokemonNumber = pokemonsNumber.shift()
-  if(pokemonNumber) {
-    const img = new Image()
-    img.src = `images/pokemons/${pokemonNumber}.png`
-    img.addEventListener('load', updateProgressbar)
-    img.addEventListener('load', () => loadInOrder())
-    img.addEventListener('error', () => console.log('Pokemon failed ' + pokemonNumber))
-    document.body.appendChild(img)
+  if (pokemonNumber) {
+    const image = new Image()
+    image.src = `images/pokemons/${pokemonNumber}.png`
+    image.addEventListener('load', updateProgressbar())
+    image.addEventListener('load', loadInOrder)
+    image.addEventListener('error', () =>
+      console.log('Pokemon failed ' + pokemonNumber)
+    )
+    document.body.appendChild(image)
   }
 }
 
